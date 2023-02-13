f1f216de198daa07d94581c80f75f9f5aba109c9
sharjil-d <sharjil.dhanani01@gmail.com>
2023-02-13 19:47:35 +0530

    changed main

--- src/main/java/jenkinsExample/JenkinsCRUD/JenkinsCrudApplication.java
+++ src/main/java/jenkinsExample/JenkinsCRUD/JenkinsCrudApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JenkinsCrudApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(JenkinsCrudApplication.class, args);
+		System.out.println("chenged here");
 	}
 
 }
